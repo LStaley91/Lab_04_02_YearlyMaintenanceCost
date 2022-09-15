7c158e54afd1cd372bacec2a8057930c4ce4713e
LStaley <lstaley9185@gmail.com>
2022-09-15 16:54:14 -0400

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double Spring = 127.3;
+        double Summer = 158.78;
+        double Fall = 92.15;
+        double Winter = 253.75;
+        double Yearly = Spring+Summer+Fall+Winter;
+        System.out.println("Springs home maintance cost is " + Spring);
+        System.out.println("Summers home maintance cost is " + Summer);
+        System.out.println("Falls home maintance cost is " + Fall);
+        System.out.println("Winters home maintance cost is " + Winter);
+        System.out.println("This years home maintance cost is " + Yearly);
+
+
     }
 }
